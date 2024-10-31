fde6fc2b2fcc61f932104f3a43f5b0e714555372
khangnl <khangnl@khangnl.tech>
2024-10-31 23:07:26 +0700

    Remove node_modules from commit

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const { v5: uuidv5 } = require('uuid');
 const app = express();
 app.use(express.json());
 
-const { API_KEY, ZALO_OA_ACCESS_TOKEN } = process.env;
+const API_KEY = 'app-BLkFz8pJ61TN1PuUsEr780xq';
+const ZALO_OA_ACCESS_TOKEN = 'zy0S56-5EXdtm14MRu4v7FJ74XHedauTgvmFSqgvGm66jXnEVOna1U6TAtvmd0mcgAnLQsYgBJMaWbyvP8K9DxwpL6abiNCGpwGkRGM9PX3yZmfePBjvBwUF5NH0oZu_k_PvOthIDJczpnb3V8n4FeM8EJTTaavZbQW-Ft6cRJNrjG505l9C5zR34ruWp4ORkCitQLh5Go-_tYyYUlu0SxVeVNiphou1ehnoUa6QN3-UlHjCO8Pi5Bl1NtnDydPqWBm4VNopA066y4nRGFu08w74VoybvHbwvTT6DJZn74lOqsCM7ECY4URcQnmZg21w_fn65GAI5qsSjZ0XMVT9Iw3kEXzEuNj_dzeN2KRAOtk3pGKGR_SY0epfU6vAtHmUWkrq64hY8tEJq6ufTSqpPKUfTnu_Qfas6m';
 const NAMESPACE = 'e8e10f1e-79b0-4c1d-98ad-8f892b12b4af'; // Một UUID cố định dùng làm namespace
 
 // Lưu conversation_id theo user_id để duy trì hội thoại
